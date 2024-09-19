92ec5eb55387eaa3b244f36d3ef6941f92ddccd2
zahra <zarakhan9490@gmail.com>
2024-09-18 22:46:49 -0700

    ui fixes

--- app/fee-structure/page.tsx
+++ app/fee-structure/page.tsx
@@ -6,7 +6,7 @@ const FeeStructure = () => {
       <h2 className="text-center text-3xl font-semibold text-kaizenblue">
         Quran classes Fee, Online Quran lessons, 3 Free trial
       </h2>
-      <p className="my-6 max-w-[900px] mx-auto text-center">
+      <p className="my-6 max-w-[900px] mx-auto text-center text-white">
         We will provide 3 free trials individual Online Quran lessons . You can
         also get individual lessons at lowest possible cost as low as 15$ per
         month in USA and £10 in UK. The below mentioned cost is exclusive of 3
@@ -35,90 +35,90 @@ const FeeStructure = () => {
           </thead>
           <tbody>
             <tr>
-              <th className="border-r-[1px] p-2 border-red-50" scope="row">
+              <th className="border-r-[1px] p-2 border-red-50  text-kaizenred" scope="row">
                 2 days a week 8 per month
               </th>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizengreen">
                 30 min lessons
               </td>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizengreen">
                 $30 per month
               </td>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizengreen">
                 £15 per month
               </td>
               <td>€18 per month</td>
             </tr>
             <tr>
-              <th className="border-r-[1px] p-2 border-red-50" scope="row">
+              <th className="border-r-[1px] p-2 border-red-50 text-kaizenred" scope="row">
                 3 days a week 12 per month
               </th>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizenred">
                 30 min lessons
               </td>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizengreen">
                 $40 per month
               </td>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizengreen">
                 £23 per month
               </td>
-              <td className="border-r-[1px] py-2 border-red-50">
+              <td className="border-r-[1px] py-2 border-red-50 text-kaizengreen">
                 €23 per month
               </td>
             </tr>
             <tr>
-              <th className="border-r-[1px] py-2 border-red-50" scope="row">
+              <th className="border-r-[1px] py-2 border-red-50 text-kaizenred" scope="row">
                 4 days a week 16 per month
               </th>
-              <td className="border-r-[1px] py-2 border-red-50">
+              <td className="border-r-[1px] py-2 border-red-50 text-kaizenred">
                 30 min lessons
               </td>
-              <td className="border-r-[1px] py-2 border-red-50">
+              <td className="border-r-[1px] py-2 border-red-50 text-kaizengreen">
                 $50 per month
               </td>
-              <td className="border-r-[1px] py-2 border-red-50">
+              <td className="border-r-[1px] py-2 border-red-50 text-kaizengreen">
                 £30 per month
               </td>
-              <td>€30 per month</td>
+              <td className="text-kaizengreen">€30 per month</td>
             </tr>
             <tr>
-              <th className="border-r-[1px] py-2 border-red-50" scope="row">
+              <th className="border-r-[1px] py-2 border-red-50 text-kaizenred" scope="row">
                 5 days a week 20 per month
               </th>
-              <td className="border-r-[1px] py-2 border-red-50">
+              <td className="border-r-[1px] py-2 border-red-50 text-kaizenred">
                 30 min lessons
               </td>
-              <td className="border-r-[1px] py-2 border-red-50">
+              <td className="border-r-[1px] py-2 border-red-50 text-kaizengreen">
                 $60 per month
               </td>
-              <td className="border-r-[1px] py-2 border-red-50">
+              <td className="border-r-[1px] py-2 border-red-50 text-kaizengreen">
                 £38 per month
               </td>
-              <td>€38 per month</td>
+              <td className="text-kaizengreen">€38 per month</td>
             </tr>
             <tr>
-              <th className="border-r-[1px] py-2 border-red-50" scope="row">
+              <th className="border-r-[1px] py-2 border-red-50 text-kaizenred" scope="row">
                 4 days a week 16 per month
               </th>
-              <td className="border-r-[1px] py-2 border-red-50">
+              <td className="border-r-[1px] py-2 border-red-50 text-kaizenred">
                 30 min lessons
               </td>
-              <td className="border-r-[1px] py-2 border-red-50">
+              <td className="border-r-[1px] py-2 border-red-50 text-kaizengreen">
                 $70 per month
               </td>
-              <td className="border-r-[1px] py-2 border-red-50">
+              <td className="border-r-[1px] py-2 border-red-50 text-kaizengreen">
                 €45 per month
               </td>
-              <td>€45 per month</td>
+              <td className="text-kaizengreen">€45 per month</td>
             </tr>
           </tbody>
         </table>
       </div>
 
-      <p className="my-6 text-center">
+      <p className="my-6 text-center text-purplish">
         We have discounts for multiple family members.
       </p>
-      <div className="max-w-7xl w-full overflow-scroll">
+      <div className="max-w-7xl w-full overflow-scroll text-white">
         <table className="table  mx-auto min-w-[1024px] border-[1px] border-red-50 text-center">
           <thead className="bg-blue-500 text-white">
             <tr className="bg-gray-800">
@@ -139,24 +139,24 @@ const FeeStructure = () => {
           </thead>
           <tbody>
             <tr>
-              <th className="border-r-[1px] py-2 border-red-50" scope="row">
+              <th className="border-r-[1px] py-2 border-red-50 text-kaizenred" scope="row">
                 3 days a week 12 per month
               </th>
-              <td className="border-r-[1px] py-2 border-red-50">
+              <td className="border-r-[1px] py-2 border-red-50 text-kaizenred">
                 90 min lessons
               </td>
-              <td className="border-r-[1px] py-2 border-red-50">
+              <td className="border-r-[1px] py-2 border-red-50 text-kaizengreen">
                 $90 per month
               </td>
-              <td className="border-r-[1px] py-2 border-red-50">
+              <td className="border-r-[1px] py-2 border-red-50 bg-kaizengreen">
                 £70 per month
               </td>
-              <td>€85 per month</td>
+              <td className="text-kaizengreen">€85 per month</td>
             </tr>
           </tbody>
         </table>
       </div>
-      <p className="my-6 text-center">
+      <p className="my-6 text-center text text-purplish">
         For Saudi Arabia,Qatar,Muscat,Oman,United Arab Emirates, Pakistan Fee
         Detail.
       </p>
@@ -181,67 +181,67 @@ const FeeStructure = () => {
           </thead>
           <tbody>
             <tr>
-              <th className="border-r-[1px] p-2 border-red-50" scope="row">
+              <th className="border-r-[1px] p-2 border-red-50 text-kaizenred" scope="row">
                 2 days a week 8 per month
               </th>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizenred">
                 30 min lessons
               </td>
               <td className="border-r-[1px] p-2 border-red-50">70 per month</td>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizengreen">
                 1500 per month
               </td>
-              <td>€15 per month</td>
+              <td className="text-kaizengreen">€15 per month</td>
             </tr>
             <tr>
-              <th className="border-r-[1px] p-2 border-red-50" scope="row">
+              <th className="border-r-[1px] p-2 border-red-50 text-kaizenred" scope="row">
                 3 days a week 12 per month
               </th>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizenred">
                 30 min lessons
               </td>
-              <td className="border-r-[1px] p-2 border-red-50">90 per month</td>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizengreen">90 per month</td>
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizengreen">
                 2000 per month
               </td>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizengreen">
                 €15 per month
               </td>
             </tr>
             <tr>
-              <th className="border-r-[1px] p-2 border-red-50" scope="row">
+              <th className="border-r-[1px] p-2 border-red-50 text-kaizenred" scope="row">
                 4 days a week 16 per month
               </th>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizenred">
                 30 min lessons
               </td>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizengreen">
                 104 per month
               </td>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizengreen">
                 2500 per month
               </td>
-              <td>€25 per month</td>
+              <td className="text-kaizengreen">€25 per month</td>
             </tr>
             <tr>
-              <th className="border-r-[1px] p-2 border-red-50" scope="row">
+              <th className="border-r-[1px] p-2 border-red-50 text-kaizenred" scope="row">
                 5 days a week 20 per month
               </th>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizenred">
                 30 min lessons
               </td>
               <td className="border-r-[1px] p-2 border-red-50">
                 115 per month
               </td>
-              <td className="border-r-[1px] p-2 border-red-50">
+              <td className="border-r-[1px] p-2 border-red-50 text-kaizengreen">
                 3000 per month
               </td>
-              <td>€30 per month</td>
+              <td className="text-kaizengreen">€30 per month</td>
             </tr>
           </tbody>
         </table>
       </div>
-      <p className="my-6 text-center w-full text-gray-950 animate-pulse bg-green-500 py-2">
+      <p className="my-6 text-center w-full text-purplish animate-pulse bg-green-500 py-2">
         We have 10% discounts for family after first student on 3 or more days
         plans.
       </p>
