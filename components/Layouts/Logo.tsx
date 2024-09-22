0f9125ddc77389c7f59ec5fca934ac97d2c4a3af
zahra <zarakhan9490@gmail.com>
2024-09-22 14:39:41 -0700

    footer responsiveness

--- components/Layouts/Logo.tsx
+++ components/Layouts/Logo.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <div className="flex items-center max-[600px]:flex-col">
+    <div className="flex items-center ">
       <div className="flex items-center">
-        <h2 className="text-purple text-xl uppercase font-normal">taealam</h2>
+        <h2 className="text-purple md:text-xl text-lg uppercase font-normal">taealam</h2>
         <div className="h-4 border-l-2 mx-2 font-bold border-white">{""}</div>
       </div>
       <h2 className="text-white text-xl uppercase font-lighter">alquran</h2>
